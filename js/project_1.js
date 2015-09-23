c101a1268ee29f74d9c12f42846256052fcdeca2
Ryan-J-Smith <ryansmith@jhu.edu>
2015-09-23 10:08:54 -0400

    Test push

--- js/project_1.js
+++ js/project_1.js
@@ -1,15 +1,16 @@
 L.mapbox.accessToken = 'pk.eyJ1IjoicnlhbmpzbWl0aCIsImEiOiJGdUZ1VllrIn0.3CnpoqP4UtaQPRHvEzssgA';
-// Construct a bounding box for this map that the user cannot move out of
+// Construct a bounding box for this map that the user cannot
+// move out of
 var southWest = L.latLng(39.196, -76.728),
     northEast = L.latLng(39.379, -76.525),
+    
     bounds = L.latLngBounds(southWest, northEast);
-
-var map = L.mapbox.map('map', 'ryanjsmith.map-xzl7zs2y', {
+var map = L.mapbox.map('map', 'ryanjsmith.i3och08o', {
     // set that bounding box as maxBounds to restrict moving the map
     // see full maxBounds documentation:
     // http://leafletjs.com/reference.html#map-maxbounds
     maxBounds: bounds,
-    maxZoom: 17,
+    maxZoom: 14,
     minZoom: 12
 });
 
