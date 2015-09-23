cfbbeb52729ef0f53fa4de537f40009415b77fa4
Ryan-J-Smith <ryansmith@jhu.edu>
2015-09-23 11:50:24 -0400

    Updated map

--- js/project_2.js
+++ js/project_2.js
@@ -4,7 +4,7 @@ var southWest = L.latLng(37.7968, -79.6179),
     northEast = L.latLng(39.8507, -74.8718),
     bounds = L.latLngBounds(southWest, northEast);
 
-var map = L.mapbox.map('map', 'rhelbing.nh6698kj', {
+var map = L.mapbox.map('map', 'rhelbing.nh6dk9ph', {
     // set that bounding box as maxBounds to restrict moving the map
     // see full maxBounds documentation:
     // http://leafletjs.com/reference.html#map-maxbounds
